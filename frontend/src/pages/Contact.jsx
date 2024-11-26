2652c27010a4a7467e91cb3658f5c8fdaafe52f5
kaanmetep <kaanpmete@gmail.com>
2024-11-26 19:57:49 +0300

    Create Contact Page

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,7 +1,30 @@
 import React from "react";
-
+import Title from "../components/Title";
+import { assets } from "../assets/frontend_assets/assets";
+import NewsletterBox from "../components/NewsletterBox";
 const Contact = () => {
-  return <div></div>;
+  return (
+    <div className="border-t">
+      <Title text1={"CONTACT"} text2={"US"} className="justify-center my-10" />
+      <div className="flex flex-col lg:flex-row gap-8 mt-10 mb-20 justify-center items-center">
+        <img
+          src={assets.contact_img}
+          alt="contact_image"
+          className="lg:w-96 w-72"
+        />
+        <div className="flex flex-col gap-4 justify-center">
+          <p className="font-semibold text-gray-700 text-lg">Our Store</p>
+          <div className="flex flex-col gap-2 text-gray-500">
+            <p>2138 Hudson Avenue</p>
+            <p>Suite 120, New York, USA</p>
+            <p>Tel: (212) 555-0184</p>
+            <p>Email: support@stark.com</p>
+          </div>
+        </div>
+      </div>
+      <NewsletterBox />
+    </div>
+  );
 };
 
 export default Contact;
